63c8e1a8435c741a87fc8ecafee20a8c0fae38fd
Cristopher <edad21@live.com>
2024-11-05 19:06:34 -0300

    Se agrega otra busqueda de indice

--- Dia 3/Indices.py
+++ Dia 3/Indices.py
@@ -7,10 +7,10 @@ print(resultado)
 
 resultado = mi_texto.index(-5) #busca la letra en el indice -5
 resultado = mi_texto.index(5)# busca la letra en el indice 5
-resultado = mi_texto.index("prueba") #busca la palabra prueba y devulelve donde inicia
-resultado = mi_texto.index("a") #busca la letra a y devulelve donde inicia
-resultado = mi_texto.index("a",5) #busca la letra a desde el indice 5 en adelantey devulelve donde inicia
-resultado = mi_texto.index("a",5,10) #busca la letra a desde el indice 5 hasta el 10 devulelve donde inicia
-resultado = mi_texto.rindex("a") #busca desde derecha a izquierda y devuelve el indice
+# resultado = mi_texto.index("prueba") #busca la palabra prueba y devulelve donde inicia
+# resultado = mi_texto.index("a") #busca la letra a y devulelve donde inicia
+# resultado = mi_texto.index("a",5) #busca la letra a desde el indice 5 en adelantey devulelve donde inicia
+# resultado = mi_texto.index("a",5,10) #busca la letra a desde el indice 5 hasta el 10 devulelve donde inicia
+# resultado = mi_texto.rindex("a") #busca desde derecha a izquierda y devuelve el indice
 
 print(resultado)
